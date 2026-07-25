a2612601003ad1e6fc734cf38b93f8fe77d68f89
Konstantinos Ioannou <konstantinosioannou@MacBook-Air.local>
2026-07-25 16:00:18 +0300

    Finish admin login, logout & change password routes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask, render_template, g, redirect, flash, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import get_db, login_required, valid_password
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -25,14 +28,12 @@ def admin():
 
 @app.route("/admin/login", methods=["GET", "POST"])
 def admin_login():
-    session.clear()
-    
     if request.method == "POST":
         if not request.form.get("username"):
-            flash("Παρακαλώ συμπληρώστε το username σας.")
+            flash("Παρακαλώ συμπληρώστε το όνομα χρήστη σας.", "danger")
             return redirect("/admin/login")
         elif not request.form.get("password"):
-            flash("Παρακαλώ όπως συμπληρώσετε τον κωδικό σας.")
+            flash("Παρακαλώ συμπληρώστε τον κωδικό σας.", "danger")
             return redirect("/admin/login")
         
         db = get_db()
@@ -44,7 +45,7 @@ def admin_login():
         if len(rows) != 1 or not check_password_hash(
             rows[0]["pass_hash"], request.form.get("password")
         ):
-            flash("Λανθασμένος συνδυασμός στοχειών.")
+            flash("Λανθασμένος συνδυασμός στοιχείων.", "danger")
             return redirect("/admin/login")
         
         session["admin_id"] = rows[0]["id"]
@@ -65,36 +66,40 @@ def logout():
 @app.route("/admin/password", methods=["GET", "POST"])
 @login_required
 def change_password():
-    curr_password = request.form.get("password")
-    new_password = request.form.get("new_password")
-    conf_password = request.form.get("confirmation")
-    
-    if not curr_password or not new_password or not conf_password:
-        flash("Παρακαλώ όπως συμπληρώσετε και τα 3 πεδία.")
-        return redirect("/admin/password")
-        
-    db = get_db()
-    
-    row = db.execute("SELECT pass_hash FROM admins WHERE id = ?", (session["admin_id"],)).fetchone()
+    if request.method == "POST":
+        curr_password = request.form.get("password")
+        new_password = request.form.get("new_password")
+        conf_password = request.form.get("confirmation")
 
-    if not check_password_hash(row["pass_hash"], curr_password):
-        flash("Λανθασμένος τρέχον κωδικός.")
-        return redirect("/admin/password")
-        
-    if not valid_password(new_password):
-        flash("Ο καινούριος κωδικός πρέπει να περιέχει τουλάχιστον 8 χαρακτήρες και τουλάχιστον απο ένα κεφαλαίο γράμμα, ψηφίο και μη αλφαριθμητικό χαρακτήρα.")
-        return redirect("/admin/password")
-    
-    if new_password != conf_password:
-        flash("Οι 2 κωδικοί πρέπει να ταιριάζουν.")
-        return redirect("/admin/password")
-    
-    db.execute("UPDATE admins SET pass_hash = ? WHERE id = ?",
-               (generate_password_hash(new_password), session["admin_id"]))
-    
-    flash("Ο κωδικός σας άλλαξε επιτυχώς.")
-    
-    return redirect("/admin")
+        if not curr_password or not new_password or not conf_password:
+            flash("Παρακαλώ συμπληρώστε και τα 3 πεδία.", "danger")
+            return redirect("/admin/password")
+
+        db = get_db()
+
+        row = db.execute("SELECT pass_hash FROM admins WHERE id = ?", (session["admin_id"],)).fetchone()
+
+        if not check_password_hash(row["pass_hash"], curr_password):
+            flash("Λανθασμένος τρέχων κωδικός.", "danger")
+            return redirect("/admin/password")
+
+        if not valid_password(new_password):
+            flash("Ο καινούριος κωδικός πρέπει να περιέχει τουλάχιστον 8 χαρακτήρες και τουλάχιστον από ένα κεφαλαίο γράμμα, ψηφίο και μη αλφαριθμητικό χαρακτήρα.", "danger")
+            return redirect("/admin/password")
+
+        if new_password != conf_password:
+            flash("Οι 2 κωδικοί πρέπει να ταιριάζουν.", "danger")
+            return redirect("/admin/password")
+
+        db.execute("UPDATE admins SET pass_hash = ? WHERE id = ?",
+                   (generate_password_hash(new_password), session["admin_id"]))
+        db.commit()
+
+        flash("Ο κωδικός σας άλλαξε επιτυχώς.", "success")
+
+        return redirect("/admin")
+    else:
+        return render_template("password.html")
         
 
 
