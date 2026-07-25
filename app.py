b07c592b41d1d55e9c11b17d78f02b476c496c53
Konstantinos Ioannou <konstantinosioannou@MacBook-Air.local>
2026-07-25 03:06:37 +0300

    Implement admin-related routes (work in progress)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, redirect, flash, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import get_db
+from helpers import get_db, login_required, valid_password
 
 app = Flask(__name__)
 
@@ -13,6 +14,88 @@ def index():
     clinic_info = db.execute("SELECT * FROM clinic_info").fetchone()
     
     return render_template("index.html", doctors=doctors, gallery=gallery, clinic_info=clinic_info)
+
+
+@app.route("/admin")
+@login_required
+def admin():
+    """TO-DO"""
+    return "TODO"
+
+
+@app.route("/admin/login", methods=["GET", "POST"])
+def admin_login():
+    session.clear()
+    
+    if request.method == "POST":
+        if not request.form.get("username"):
+            flash("Παρακαλώ συμπληρώστε το username σας.")
+            return redirect("/admin/login")
+        elif not request.form.get("password"):
+            flash("Παρακαλώ όπως συμπληρώσετε τον κωδικό σας.")
+            return redirect("/admin/login")
+        
+        db = get_db()
+        
+        rows = db.execute(
+            "SELECT * FROM admins WHERE username = ?", (request.form.get("username"),)
+        ).fetchall()
+        
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["pass_hash"], request.form.get("password")
+        ):
+            flash("Λανθασμένος συνδυασμός στοχειών.")
+            return redirect("/admin/login")
+        
+        session["admin_id"] = rows[0]["id"]
+        
+        return redirect("/admin")
+    else:
+        return render_template("login.html")
+            
+    
+
+@app.route("/admin/logout")
+def logout():
+    session.clear()
+    
+    return redirect("/")
+
+    
+@app.route("/admin/password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    curr_password = request.form.get("password")
+    new_password = request.form.get("new_password")
+    conf_password = request.form.get("confirmation")
+    
+    if not curr_password or not new_password or not conf_password:
+        flash("Παρακαλώ όπως συμπληρώσετε και τα 3 πεδία.")
+        return redirect("/admin/password")
+        
+    db = get_db()
+    
+    row = db.execute("SELECT pass_hash FROM admins WHERE id = ?", (session["admin_id"],)).fetchone()
+
+    if not check_password_hash(row["pass_hash"], curr_password):
+        flash("Λανθασμένος τρέχον κωδικός.")
+        return redirect("/admin/password")
+        
+    if not valid_password(new_password):
+        flash("Ο καινούριος κωδικός πρέπει να περιέχει τουλάχιστον 8 χαρακτήρες και τουλάχιστον απο ένα κεφαλαίο γράμμα, ψηφίο και μη αλφαριθμητικό χαρακτήρα.")
+        return redirect("/admin/password")
+    
+    if new_password != conf_password:
+        flash("Οι 2 κωδικοί πρέπει να ταιριάζουν.")
+        return redirect("/admin/password")
+    
+    db.execute("UPDATE admins SET pass_hash = ? WHERE id = ?",
+               (generate_password_hash(new_password), session["admin_id"]))
+    
+    flash("Ο κωδικός σας άλλαξε επιτυχώς.")
+    
+    return redirect("/admin")
+        
 
 
 @app.teardown_appcontext
--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import sqlite3
 
-from flask import g
+from flask import g, redirect, session
+from functools import wraps
 
 def get_db():
     if "db" not in g:
@@ -8,4 +9,31 @@ def get_db():
         g.db.row_factory = sqlite3.Row
         
     return g.db
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("admin_id") is None:
+            return redirect("/admin/login")
         
+        return f(*args, **kwargs)
+    
+    return decorated_function
+        
+        
+def valid_password(password):
+    if password is None or len(password) < 8:
+        return False
+    
+    has_number = has_uppercase = has_special = False
+    
+    for char in password:
+        if char.isdigit():
+            has_number = True
+        elif char.isupper():
+            has_uppercase = True
+        elif not char.isalnum():
+            has_special = True
+            
+    return has_number and has_uppercase and has_special
