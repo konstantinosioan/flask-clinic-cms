8f4ce6c1f3a907384b1daa80afc63231765aa990
Konstantinos Ioannou <konstantinosioannou@MacBook-Air.local>
2026-07-26 01:15:07 +0300

    Add being able to see an image preview before confirming changes using JS. Make the navbar's content visible on smaller screens with JS turned off.

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
 function confirmDeletion() {
-    let forms = document.querySelectorAll('.confirm-delete');
+    const forms = document.querySelectorAll('.confirm-delete');
 
     for (const form of forms) {
-        form.addEventListener('submit', function(event) {
+        form.addEventListener('submit', (event) => {
             if (!confirm('Θέλετε σίγουρα να διαγράψετε αυτό το στοιχείο;')) {
                 event.preventDefault();
             }
@@ -11,3 +11,27 @@ function confirmDeletion() {
 }
 
 confirmDeletion();
+
+
+function showPreview() {
+    const fileInputs = document.querySelectorAll('.photo-input');
+
+    for (const fileInput of fileInputs) {
+        fileInput.addEventListener('change', () => {
+            const file = fileInput.files[0];
+            const preview = document.getElementById(fileInput.id + '-preview');
+            const reader = new FileReader();
+
+            reader.addEventListener('load', () => {
+                preview.src = reader.result;
+                preview.classList.remove('d-none');
+            });
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        });
+    }
+}
+
+showPreview();
